bddd5d08adf1a3b32994492432cad7710f0c4efe
plaguemaster <65495621+Plaguemaster@users.noreply.github.com>
2022-06-03 20:35:30 +0300

    Update equation.cpp

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -3,35 +3,28 @@
 #include "formatter_ex.h"
 #include "solver.h"
 
-int
-main(int argc, char* argv[])
+int main()
 {
-  float a = 0;
-  for (size_t i = 0; argv[1][i] != '\0'; ++i) {
-    a = a * 10 + (float(argv[1][i]) - 48);
-  }
-  float b = 0;
-  for (size_t i = 0; argv[2][i] != '\0'; ++i) {
-    b = b * 10 + (float(argv[2][i]) - 48);
-  }
-  float c = 0;
-  for (size_t i = 0; argv[3][i] != '\0'; ++i) {
-    c = c * 10 + (float(argv[3][i]) - 48);
-  }
+    float a = 0;
+    float b = 0;
+    float c = 0;
+   
+    std::cin >> a >> b >> c;
+    
+    float x1 = 0;
+    float x2 = 0;
 
-  std::cout << a << " " << b << " " << c << std::endl;
+    try
+    {
+        solve(a, b, c, x1, x2);
 
-  float x1 = 0;
-  float x2 = 0;
+        formatter(std::cout, "x1 = " + std::to_string(x1));
+        formatter(std::cout, "x2 = " + std::to_string(x2));
+    }
+    catch (const std::logic_error& ex)
+    {
+        formatter(std::cout, ex.what());
+    }
 
-  try {
-    solve(a, b, c, x1, x2);
-
-    formatter(std::cout, "x1 = " + std::to_string(x1));
-    formatter(std::cout, "x2 = " + std::to_string(x2));
-  } catch (const std::logic_error& ex) {
-    formatter(std::cout, ex.what());
-  }
-
-  return 0;
+    return 0;
 }
